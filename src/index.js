a683e3d2aea976a0aec20c843d693c42bea0ab86
knzphumthawan <knzphumthawan@gmail.com>
2020-02-19 06:48:25 +0700

    install react redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducer, composeEnhancers(middleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,2 @@
 export { default as Dashboard } from './Dashboard';
-export { default as Icons } from './Icons';
 export { default as NotFound } from './NotFound';
