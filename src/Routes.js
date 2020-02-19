909d2dac99480436eeff16370140379e5582a4c4
knzphumthawan <knzphumthawan@gmail.com>
2020-02-19 22:15:22 +0700

    install route login

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,22 @@ import { Switch, Redirect } from 'react-router-dom';
 import { RouteWithLayout } from './components';
 import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
 
-import { Dashboard as DashboardView, NotFound as NotFoundView } from './views';
+import {
+  Dashboard as DashboardView,
+  NotFound as NotFoundView,
+  SignIn as SignInView
+} from './views';
 
 const Routes = () => {
   return (
     <Switch>
       <Redirect exact from="/" to="/dashboard" />
+      <RouteWithLayout
+        component={SignInView}
+        exact
+        layout={MinimalLayout}
+        path="/sign-in"
+      />
       <RouteWithLayout
         component={DashboardView}
         exact
