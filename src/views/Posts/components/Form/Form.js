2847030cd517577c5a9b25ef274e5f5c4dcf567b
Phumthawan Nokun <phumthawan.nok@ascendcorp.com>
2020-03-05 16:31:37 +0700

    form layout

--- src/views/Posts/components/Form/Form.js
+++ src/views/Posts/components/Form/Form.js
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import validate from 'validate.js';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/styles';
+import React, { useState, useEffect } from "react";
+import validate from "validate.js";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/styles";
 import {
-  Card,
   CardHeader,
-  CardContent,
-  CardActions,
   Divider,
   TextField,
   Typography,
-  Button
-} from '@material-ui/core';
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState, convertToRaw } from 'draft-js';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import '../Form/css/style.css';
-import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
+  Button,
+  Grid,
+  Paper
+} from "@material-ui/core";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../Form/css/style.css";
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'block',
-    flexWrap: 'wrap'
+    flexGrow: 1
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -32,19 +30,29 @@ const useStyles = makeStyles(theme => ({
   },
   formField: {
     marginTop: theme.spacing(2)
+  },
+  paperCenter: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  paperLeft: {
+    padding: theme.spacing(2),
+    textAlign: "left",
+    color: theme.palette.text.secondary
   }
 }));
 
 const schema = {
   slug: {
-    presence: { allowEmpty: false, message: 'is required' },
+    presence: { allowEmpty: false, message: "is required" },
     length: {
       maximum: 4,
       minimum: 4
     }
   },
   title: {
-    presence: { allowEmpty: false, message: 'is required' },
+    presence: { allowEmpty: false, message: "is required" },
     length: {
       maximum: 128,
       minimum: 6
@@ -82,7 +90,7 @@ const Form = props => {
       values: {
         ...formState.values,
         [event.target.name]:
-          event.target.type === 'checkbox'
+          event.target.type === "checkbox"
             ? event.target.checked
             : event.target.value
       },
@@ -91,16 +99,16 @@ const Form = props => {
         [event.target.name]: true
       }
     }));
-    console.log(formState);
   };
 
-  const [blogText, setBlogText] = useState(EditorState.createEmpty());
+  const [body, setBobdy] = useState(EditorState.createEmpty());
 
   const onEditorStateChange = editorState => {
-    setBlogText(editorState);
+    setBobdy(editorState);
   };
 
   const handleSubmit = event => {
+    console.log(formState.values);
     event.preventDefault();
   };
 
@@ -108,76 +116,89 @@ const Form = props => {
     formState.touched[field] && formState.errors[field] ? true : false;
 
   return (
-    <Card {...rest} className={clsx(classes.root, className)}>
-      <form onSubmit={handleSubmit}>
-        <CardHeader subheader="Manage the posts" title="Posts" />
-        <Divider />
-        <CardContent>
-          <div>
-            <TextField
-              id="slug"
-              label="Slug"
-              style={{ margin: 5 }}
+    <div {...rest} className={clsx(classes.root, className)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+        <form onSubmit={event => handleSubmit(event)}>
+          <Paper className={classes.paperCenter}>
+              <CardHeader subheader="Create your posts" title="Posts" />
+              <Divider />
+              <div>
+                <TextField
+                  id="slug"
+                  label="Slug"
+                  style={{ margin: 5 }}
+                  fullWidth
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  className={classes.formField}
+                  error={hasError("slug")}
+                  helperText={
+                    hasError("slug") ? formState.errors.slug[0] : null
+                  }
+                  onChange={handleChange}
+                  type="text"
+                  name="slug"
+                  value={formState.values.slug || ""}
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <TextField
+                  id="title"
+                  label="Title"
+                  style={{ margin: 5 }}
+                  fullWidth
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  className={classes.formField}
+                  error={hasError("title")}
+                  helperText={
+                    hasError("title") ? formState.errors.title[0] : null
+                  }
+                  onChange={handleChange}
+                  type="text"
+                  name="title"
+                  value={formState.values.title || ""}
+                  variant="outlined"
+                />
+              </div>
+              <div>
+                <Editor
+                  onEditorStateChange={editorState =>
+                    onEditorStateChange(editorState)
+                  }
+                  toolbarClassName="rdw-storybook-toolbar"
+                  wrapperClassName="rdw-storybook-wrapper"
+                  editorClassName="rdw-storybook-editor"
+                />
+              </div>
+              {/*
+              <div>
+                {draftToHtml(convertToRaw(body.getCurrentContent()))}
+              </div>
+              */}
+              <Divider />
+          </Paper>
+          <Paper className={classes.paperLeft}>
+            <Button
+              color="primary"
+              disabled={!formState.isValid}
               fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true
-              }}
-              className={classes.formField}
-              error={hasError('slug')}
-              helperText={hasError('slug') ? formState.errors.slug[0] : null}
-              onChange={handleChange}
-              type="text"
-              name="slug"
-              value={formState.values.slug || ''}
-              variant="outlined"
-            />
-          </div>
-          <div>
-            <TextField
-              id="title"
-              label="Title"
-              style={{ margin: 5 }}
-              fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true
-              }}
-              className={classes.formField}
-              error={hasError('title')}
-              helperText={hasError('title') ? formState.errors.title[0] : null}
-              onChange={handleChange}
-              type="text"
-              name="title"
-              value={formState.values.title || ''}
-              variant="outlined"
-            />
-          </div>
-          <div>
-            <Editor
-              onEditorStateChange={editorState =>
-                onEditorStateChange(editorState)
-              }
-              toolbarClassName="rdw-storybook-toolbar"
-              wrapperClassName="rdw-storybook-wrapper"
-              editorClassName="rdw-storybook-editor"
-            />
-          </div>
-          <div>{draftToHtml(convertToRaw(blogText.getCurrentContent()))}</div>
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button
-            color="primary"
-            variant="outlined"
-            disabled={!formState.isValid}
-            type="submit"
-          >
-            Save
-          </Button>
-        </CardActions>
-      </form>
-    </Card>
+              type="submit"
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Paper>
+          </form>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
